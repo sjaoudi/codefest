7bbe1f2532fb4a11784460cbb8b5b28a7c6893eb
U-AWC-THINKPAD\Andrew <aconant185@comcast.net>
2016-02-20 18:15:04 -0500

    fixed old reference to condom

--- codefest/healthphilly/admin.py
+++ codefest/healthphilly/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import CondomDistributor
+from .models import Location
 # Register your models here.
 
-admin.site.register(CondomDistributor)
-#admin.site.register(HealthyStartCRC)
-#admin.site.register(WICOffice)
+admin.site.register(Location)
