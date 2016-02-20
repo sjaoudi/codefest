3bfb03758435425f2631a1ce86e10868a8d0fcdb
Anna Jensen <ajensen1@swarthmore.edu>
2016-02-20 17:32:14 -0500

    try

--- codefest/healthphilly/urls.py
+++ codefest/healthphilly/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^', views.index, name='index'),
+    url(r'^about', views.about, name='about'),
+    url(r'^search', views.searchpage, name='search'),
 ]
