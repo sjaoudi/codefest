245e0ca9c0407ba4bb48cfc7a5e5d8752e2ba4af
Anna Jensen <ajensen1@swarthmore.edu>
2016-02-20 17:42:30 -0500

    still broken shrug emoji

--- codefest/healthphilly/urls.py
+++ codefest/healthphilly/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-    url(r'^about', views.about, name='about'),
-    url(r'^search', views.searchpage, name='search'),
+    url(r'', views.index, name='index'),
+    url(r'^#/about', views.about, name='about'),
+    url(r'^#/search', views.searchpage, name='search'),
 ]
