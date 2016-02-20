48afc8464f12b06b7a79c55ff7dd31d3a93004a9
dwurtel1 <dwurtel1@swarthmore.edu>
2016-02-20 16:10:00 -0500

    updated gitignore to stop moving migrations, added healthystartCRC and wicoffice to admin.py

--- codefest/healthphilly/admin.py
+++ codefest/healthphilly/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CondomDistributor
 # Register your models here.
+
+admin.site.register(CondomDistributor)
+admin.site.register(HealthyStartCRC)
+admin.site.register(WICOffice)
