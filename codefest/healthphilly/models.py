019806ce18682b1a69c63607a9284486cd4d7e43
dwurtel1 <dwurtel1@swarthmore.edu>
2016-02-20 15:42:08 -0500

    added healthphilly to apps and made very base model for condomdistributors

--- codefest/healthphilly/models.py
+++ codefest/healthphilly/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class CondomDistributor(models.Model):
+	business_name = models.CharField(default='', max_length=300)
+	pub_date = models.DateTimeField('date published')
+	longitude = models.CharField(default='')
