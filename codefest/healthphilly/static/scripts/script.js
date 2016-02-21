2cbc52bc93920e25abec0ce7c88b01f0fc0da2d9
dwurtel1 <dwurtel1@swarthmore.edu>
2016-02-21 01:47:30 -0500

    finally starting to work on ajax

--- codefest/healthphilly/static/scripts/script.js
+++ codefest/healthphilly/static/scripts/script.js
@@ -1,33 +1,31 @@
+$(function() {
+	console.log("listening!");
+	$("input[type='checkbox']").on("change", function() {
+		alert("Change to " + this.value);
+	});
+	console.log("yo");
+	$('test').on('change', function(event) {
+		console.log("i've been hit!");
+		var hitID = evt.target.id;
+		
+		if ($('#'+hitID).is(":checked")) {
+			$.ajax({
+				url: 'search',
+				type: 'POST',
+				data: { "strID":hitID, "state":"1" }
+			});
+		} else {
+			$.ajax({
+				url: 'search',
+				type: 'POST',
+				data: { "strID":hitID, "state":"0" }
+			});
+		}
+	});
+
+});
 
 
-var searchFormScript = (function() {
-	var addEventListeners = function() {
-		console.log("listening!");
-		$("#choices").on("input", function() {
-    		alert("Change to " + this.value);
-		});
-		console.log("yo");
-		$('input:checkbox').on('change', function(event) {
-			console.log("i've been hit!");
-			var hitID = evt.target.id;
-			
-			if ($('#'+hitID).is(":checked")) {
-				$.ajax({
-					url: 'search',
-					type: 'POST',
-					data: { "strID":hitID, "state":"1" }
-				});
-			} else {
-				$.ajax({
-					url: 'search',
-					type: 'POST',
-					data: { "strID":hitID, "state":"0" }
-				});
-			}
-		});
 
-	}
-	return {
-		addEventListeners : addEventListeners()
-	};
-})();
+
+
